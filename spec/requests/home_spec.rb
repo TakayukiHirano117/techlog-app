1caac48c65ea17bc556176f2bf3ae8c89cc2b6a3
hirano-ta <hirano-ta@ibjapan.jp>
2025-05-30 11:14:53 +0900

    Home/topのテスト作成

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
-  describe "GET /top" do
-    it "returns http success" do
+  describe "GET /" do
+    it "HTTPステータス200を返す" do
       get "/home/top"
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(200)
     end
   end
 
